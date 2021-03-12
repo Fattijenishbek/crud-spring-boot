99cb5e84536b27d34419e8c0b28f1b8eb3b9de6d
Fattijenishbek <fattijenishbek01@gmail.com>
2021-03-13 02:52:19 +0600

    redirect if id doesn't exist

--- src/main/java/com/example/cscrudapp/controllers/BlogController.java
+++ src/main/java/com/example/cscrudapp/controllers/BlogController.java
@@ -40,6 +40,9 @@ public class BlogController {
 
     @GetMapping("/blog/{id}")
     public String blogDetails(@PathVariable(value = "id") long id, Model model ){
+        if (!postRepository.existsById(id)){
+            return "redirect:/blog";
+        }
         Optional<Post> post = postRepository.findById(id);
         ArrayList<Post> result = new ArrayList<>();
         post.ifPresent(result::add);
