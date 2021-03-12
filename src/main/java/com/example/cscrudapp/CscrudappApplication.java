30d11151952404df16ec5f3e399fe16af1961d0e
Fattijenishbek <fattijenishbek01@gmail.com>
2021-03-12 19:52:56 +0600

    create projct

---
+++ src/main/java/com/example/cscrudapp/CscrudappApplication.java
@@ -0,0 +1,13 @@
+package com.example.cscrudapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CscrudappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CscrudappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cscrudapp/CscrudappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cscrudapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CscrudappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
